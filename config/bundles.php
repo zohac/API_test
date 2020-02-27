2a405d883710e6c63941b82853df5015b44f4b90
s.jouan <simon@jouan.ovh>
2020-02-27 18:14:51 +0100

    [feat]: install of the symfony WebProfilerBundle, and the MakerBundle for the development

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
 ];
