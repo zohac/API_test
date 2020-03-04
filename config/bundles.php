9ee2e622d7ca48d8abef83f784a409deb57f961e
s.jouan <simon@jouan.ovh>
2020-03-04 14:12:37 +0100

    [feat]: create the MakerBundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
+    // Cins MakerBundle
+    App\MakerBundle\MakerBundle::class => ['all' => true],
 ];
---
+++ src/MakerBundle/MakerBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+// src/AcmeBundle/AcmeBundle.php
+namespace App\MakerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MakerBundle extends Bundle
+{
+}
